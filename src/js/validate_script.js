408890f0c2f517c74b862ad1bb2e793f44c322ed
Dmitry <dmitry1939@gmail.com>
2016-05-19 11:05:41 +0300

    header-bottom checkpoint added

--- src/js/validate_script.js
+++ src/js/validate_script.js
@@ -25,19 +25,19 @@ function validate(form, options){
                 }
             },
             errorPlacement: function(error, element) {
-                error.appendTo( element.closest('.form_input'));
+                error.appendTo( element.closest('.form-input'));
             },
             highlight: function(element, errorClass, validClass) {
                 $(element).addClass('error');
-                $(element).closest('.form_row').addClass('error').removeClass('valid');
+                $(element).closest('.form-row').addClass('error').removeClass('valid');
                 if( typeof(setings.highlightFunction) === 'function' ) {
                     setings.highlightFunction(form);
                 }
             },
             unhighlight: function(element, errorClass, validClass) {
                 $(element).removeClass('error');
-                if($(element).closest('.form_row').is('.error')){
-                    $(element).closest('.form_row').removeClass('error').addClass('valid');
+                if($(element).closest('.form-row').is('.error')){
+                    $(element).closest('.form-row').removeClass('error').addClass('valid');
                 }
                 if( typeof(setings.unhighlightFunction) === 'function' ) {
                     setings.unhighlightFunction(form);
@@ -268,6 +268,7 @@ $(document).ready(function(){
    validate('#call-popup .contact-form', {submitFunction:validationCall});
 
    validate('.login-form-enter');
+   validate('.search-form');
 
    Maskedinput();
    fancyboxForm();
