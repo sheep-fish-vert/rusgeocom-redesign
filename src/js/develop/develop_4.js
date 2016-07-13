f4bd38089e701ce63322202639917f7ae4d756a3
radiks <radion91@gmail.com>
2016-07-13 14:21:14 +0300

    map fix

--- src/js/develop/develop_4.js
+++ src/js/develop/develop_4.js
@@ -566,50 +566,33 @@ function rewiewFancybox(){
 
 }
 
-function googleMap(mapWrap){
+function googleMap(){
     function initialize() {
         var myLatlng = new google.maps.LatLng(cordX,cordY);
         var myOptions = {
             zoom: 16,
-            center: myLatlng,
-            disableDefaultUI: false, //без управляющих елементов
-            mapTypeId: google.maps.MapTypeId.ROADMAP, // SATELLITE - снимки со спутника,
-            zoomControlOptions: {
-               position: google.maps.ControlPosition.LEFT_BOTTOM // позиция слева внизу для упр елементов
-            }
+            center: myLatlng
+            // disableDefaultUI: false, //без управляющих елементов
+            // mapTypeId: google.maps.MapTypeId.ROADMAP, // SATELLITE - снимки со спутника,
+            // zoomControlOptions: {
+            //    position: google.maps.ControlPosition.LEFT_BOTTOM // позиция слева внизу для упр елементов
+            // }
         }
-        var map = new google.maps.Map(document.getElementById(mapWrap), myOptions);
+        var map = new google.maps.Map(document.getElementById('map'), myOptions);
 
         // var contentString = '<div class="marker-test">'+googleText+'</div>';
         // var infowindow = new google.maps.InfoWindow({
         //     content: contentString
         // });
 
-
-        /*маркер на svg*/
-        var SQUARE_PIN = 'M0-48c-9.8 0-17.7 7.8-17.7 17.4 0 15.5 17.7 30.6 17.7 30.6s17.7-15.4 17.7-30.6c0-9.6-7.9-17.4-17.7-17.4z'
-        //больше - http://map-icons.com/
-        /*/маркер на svg*/
-
         //var image = 'images/footer-contact-marker.png';   // иконка картинкой
 
         var marker = new google.maps.Marker({
             position: myLatlng,
             map: map,
-            animation: google.maps.Animation.DROP, // анимация при загрузке карты
             //icon: image //  иконка картинкой
         });
 
-        /*анимация при клике на маркер*/
-        marker.addListener('click', toggleBounce);
-        function toggleBounce() {
-          if (marker.getAnimation() !== null) {
-            marker.setAnimation(null);
-          } else {
-            marker.setAnimation(google.maps.Animation.BOUNCE);
-          }
-        }
-        /*/анимация при клике на маркер*/
 
         /*По клику открываеться инфоблок*/
         google.maps.event.addListener(marker, 'click', function() {
@@ -636,11 +619,11 @@ $(document).ready(function(){
   sliderBends();
   ratingScript();
 
-  googleMap('#map');
+
 });
 
 $(window).load(function(){
-
+  googleMap();
 });
 
 $(window).resize(function(){
